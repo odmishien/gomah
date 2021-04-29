2f242715b03f1daf244b0ee67cbcabb0f45268d2
odmishien <tmtm.yagi.007@gmail.com>
2021-04-29 17:14:29 +0900

    fix test

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -11,10 +11,12 @@ func TestRoot(t *testing.T) {
 		name    string
 		command string
 		want    string
+		wantErr bool
 	}{
-		{"長いやつ", "gomah -m 123123123123 -s 123 -p 123 -w 111", "🀇 🀈 🀉 🀇 🀈 🀉 🀇 🀈 🀉 🀇 🀈 🀉 🀐 🀑 🀒 🀙 🀚 🀛 🀀 🀀 🀀 \n"},
-		{"普通のやつ", "gomah -m 123 -s 123 -p 123 -w 111 -d 22", "🀇 🀈 🀉 🀐 🀑 🀒 🀙 🀚 🀛 🀀 🀀 🀀 🀅 🀅 \n"},
-		{"短いやつ", "gomah -m 123", "🀇 🀈 🀉 \n"},
+		{"長いやつ", "gomah -m 123123123123 -s 123 -p 123 -w 111", "🀇 🀈 🀉 🀇 🀈 🀉 🀇 🀈 🀉 🀇 🀈 🀉 🀐 🀑 🀒 🀙 🀚 🀛 🀀 🀀 🀀 \n", false},
+		{"普通のやつ", "gomah -m 123 -s 123 -p 123 -w 111 -d 22", "🀇 🀈 🀉 🀐 🀑 🀒 🀙 🀚 🀛 🀀 🀀 🀀 🀅 🀅 \n", false},
+		{"短いやつ", "gomah -m 123", "🀇 🀈 🀉 \n", false},
+		{"存在しない牌の種類は落ちる", "gomah -w 5", "", true},
 	}
 	for _, tc := range tcs {
 		buf := new(bytes.Buffer)
@@ -22,10 +24,13 @@ func TestRoot(t *testing.T) {
 		cmd.SetOutput(buf)
 		cmdArgs := strings.Split(tc.command, " ")
 		cmd.SetArgs(cmdArgs[1:])
-		cmd.Execute()
+		err := cmd.Execute()
+		if tc.wantErr && err == nil {
+			t.Errorf("expected error, but not returned error")
+		}
 
 		get := buf.String()
-		if tc.want != get {
+		if !tc.wantErr && tc.want != get {
 			t.Errorf("unexpected response: want:%+v, get:%+v", tc.want, get)
 		}
 	}
